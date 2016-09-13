57721633305c5b4a6b58e8c2ca6e1ce690e1a331
哒哒啊 <1106541135@qq.com>
2016-09-13 20:01:23 +0800

    composer test

---
+++ app/controller/user.php
@@ -0,0 +1,12 @@
+<?php namespace app\controller;
+/**
+ * Created by PhpStorm.
+ * User: 哒哒爱璐璐
+ * Date: 2016/9/13
+ * Time: 19:45
+ */
+class User {
+    public function show() {
+        echo '武飞达 1106541135@qq.com';
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 哒哒爱璐璐
+ * Date: 2016/9/13
+ * Time: 19:47
+ */
+require 'vendor/autoload.php';
+use app\controller\user;
+$obj = new User;
+$obj->show();
